8f499798c1e6fbe9d250d3ff703deccaf6139258
02konan <152191746+02konan@users.noreply.github.com>
2025-05-01 23:23:52 +0200

    Mise à jour de l'URI de la base de données PostgreSQL dans la configuration de l'application

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 # import detecteur
 
 app= Flask(__name__, static_folder='static', template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('postgresql://ifsm_db_user:kGi5McF1aQchxW7D712d71Lh1E2TUKdc@dpg-d09sq1uuk2gs73cqb6fg-a/ifsm_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
