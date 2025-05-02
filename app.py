a0efdd62b5da4a83e79ba680ea7e370d655dc8fd
02konan <152191746+02konan@users.noreply.github.com>
2025-05-02 17:34:29 +0200

    Refactor dashboard and sidebar components: update data retrieval, enhance UI with new statistics cards, and implement modular HTML includes for better maintainability.

--- app.py
+++ app.py
@@ -18,20 +18,18 @@ CORS(app)
 
 @app.route('/')
 def index():
-    # Appel de la fonction pour lire les données de la base de données
     data = read_data_from_db()
-    list_donnee = data
-    employees = []  
-    if list_donnee:
-        for list_donnes in list_donnee:
-            employee = {
-                'id': list_donnes[0],
-                'user_id': list_donnes[1],
-                'nom': list_donnes[2],
-                'finger_id': list_donnes[3],
-                'template': list_donnes[4]
-            }
-            employees.append(employee)
+    if data:
+        total_eleves, total_Presents, total_retard, activité_recentes = data
+        employee={
+            'total_eleves': total_eleves,
+            'presents': total_Presents,
+            'retard': total_retard,
+            'activité_recentes':activité_recentes,
+        }
+        employees=[employee]
+    else:
+        employees=[]
     return render_template('index.html', employee_data=employees)
 
 
--- read_data.py
+++ read_data.py
@@ -11,15 +11,31 @@ def read_data_from_db():
         )
 
         with data_base.cursor() as cursor:
-            # Requête SQL CORRECTE avec WHERE
-            sql = "SELECT * FROM empreintes WHERE user_id = %s"
-            user_id = 164
-            cursor.execute(sql, (user_id,))
-            return cursor.fetchall()
+            # Requête POUR AFFICHER LE NOMBRE D'EMPLOYES
+            sql1 = "SELECT COUNT(nom) FROM empreintes"
+            sql2 = "SELECT COUNT(*) FROM `empreintes_utilisees` WHERE time(heure_pointage) < '08:00:00'"
+            sql3 = "SELECT COUNT(*) FROM `empreintes_utilisees` WHERE time(heure_pointage) > '08:00:00'"
+            sql4 = "SELECT DISTINCT empreintes.user_id,heure_pointage " \
+            "FROM empreintes_utilisees " \
+            "JOIN empreintes on empreintes_utilisees.user_id=empreintes.user_id" \
+            " ORDER BY heure_pointage DESC LIMIT 4;"
+
+
+            cursor.execute(sql1)
+            total_eleves=cursor.fetchone()[0]
+            cursor.execute(sql2)
+            total_Presents=cursor.fetchone()[0]
+            cursor.execute(sql3)
+            total_retard=cursor.fetchone()[0]
+            cursor.execute(sql4)
+            activité_recentes=cursor.fetchall()
+
+            return total_eleves,total_Presents,total_retard,activité_recentes
         data_base.close()
 
     except pymysql.MySQLError as e:
         print("Erreur MySQL :", e)
     except Exception as e:
         print("Erreur générale :", e)
+# print(read_data_from_db)
 
