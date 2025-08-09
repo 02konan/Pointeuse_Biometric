559dd7e8e2773322b2f81a624b4c851f562be3e7
02konan <152191746+02konan@users.noreply.github.com>
2025-08-09 20:56:27 +0000

    postgrre

--- base_donnee.py
+++ base_donnee.py
@@ -2,10 +2,7 @@ import pymysql
 import os
 
 def connexion():
-    return pymysql.connect(
-        host=os.environ['DB_HOST'], 
-        port=3306,
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASS'],
-        database=os.environ['DB_NAME']
-    )
+    host = '91.216.107.248'
+    dbname = 'ifsme390267_4hts3c'
+    username = 'ifsme390267'
+    password = 'FSYP7AkY'
