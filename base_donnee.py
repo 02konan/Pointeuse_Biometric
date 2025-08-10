96e46b35d0e1f03011478ce674429e2520612dec
02konan <152191746+02konan@users.noreply.github.com>
2025-08-10 22:54:47 +0000

    mise en ligne avec alwaysdata

--- base_donnee.py
+++ base_donnee.py
@@ -2,7 +2,7 @@ import pymysql
 import os
 
 def connexion():
-    host = '91.216.107.248'
-    dbname = 'ifsme390267_4hts3c'
-    username = 'ifsme390267'
-    password = 'FSYP7AkY'
+    host = 'mysql-biopro.alwaysdata.net'
+    dbname = 'biopro_db'
+    username = 'biopro'
+    password ='BioPro205ifsm'
