3f189fa55c7309090b19b56c6c70ff786eacd2a0
02konan <152191746+02konan@users.noreply.github.com>
2025-08-23 19:34:29 +0000

    base de donnee

--- programme/base_donnee.py
+++ programme/base_donnee.py
@@ -1,20 +1,20 @@
 import pymysql
 import os
 
-# def connexion():
-#     connection = pymysql.connect(
-#         host='mysql-biopro.alwaysdata.net',
-#         db='biopro_db',
-#         user='biopro',
-#         password='BioPro205ifsm'
-#     )
-#     return connection
 def connexion():
     connection = pymysql.connect(
-        host='localhost',
-        db='bioprodb',
-        user='root',
-        password=''
+        host='mysql-biopro.alwaysdata.net',
+        db='biopro_db',
+        user='biopro',
+        password='BioPro205ifsm'
     )
     return connection
+# def connexion():
+#     connection = pymysql.connect(
+#         host='localhost',
+#         db='bioprodb',
+#         user='root',
+#         password=''
+#     )
+#     return connection
     
