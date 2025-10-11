1dbbe83a1294e35af2032093484cf4344ea8b9c5
02konan <152191746+02konan@users.noreply.github.com>
2025-10-11 10:34:13 +0000

    revision programme

--- programme/base_donnee.py
+++ programme/base_donnee.py
@@ -1,14 +1,14 @@
 import pymysql
 import os
 
-# def connexion():
-#     connection = pymysql.connect(
-#         host='mysql-divix.alwaysdata.net',
-#         db='divix_bd',
-#         user='divix',
-#         password='Biometricifsm@2025'
-#     )
-#     return connection
+def connexion():
+    connection = pymysql.connect(
+        host='mysql-divix.alwaysdata.net',
+        db='divix_bd',
+        user='divix',
+        password='Biometricifsm@2025'
+    )
+    return connection
 
 # def connexion():
 #     connection = pymysql.connect(
